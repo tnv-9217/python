c4e8456c53fee4f1b83dd5d8fda6c64113f8479c
tnv-9217 <67188868+tnv-9217@users.noreply.github.com>
2021-09-29 18:22:52 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,14 @@
+stack = []
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('initial stack')
+print(stack)
+
+print('\n elements popped from stack\n')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack)
